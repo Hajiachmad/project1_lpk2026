6bbfba70683def8801089a2c2bce7f802e50f0fd
Hajiachmad <ahmdfai0708@gmail.com>
2026-05-22 14:21:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,16 @@ import streamlit as st
 st.title("🎈 Project kela a")
 import streamlit as st
 
-md = st.text_area('Type in your markdown string (without outer quotes)',
-                  "Happy Streamlit-ing! :balloon:")
+st.markdown("*Streamlit* is **really** ***cool***.")
+st.markdown('''
+    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
 
-st.code(f"""
-import streamlit as st
+multi = '''If you end a line with two spaces,
+a soft return is used for the next line.
 
-st.markdown('''{md}''')
-""")
-
-st.markdown(md)
+Two (or more) newline characters in a row will result in a hard return.
+'''
+st.markdown(multi)
