48307b7c0fd4d7236e28b1c36d74f652ec106669
Hajiachmad <ahmdfai0708@gmail.com>
2026-05-22 14:25:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,25 +3,11 @@ import streamlit as st
 st.title("🎈 Project kela a")
 import streamlit as st
 
-st.markdown("*Streamlit* is **really** ***cool***.")
-st.markdown('''
-    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
-    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
-st.markdown("Here's a bouquet &mdash;\
-            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+a, b = st.columns(2)
+c, d = st.columns(2)
 
-multi = '''If you end a line with two spaces,
-a soft return is used for the next line.
+a.metric("Temperature", "30°F", "-9°F", border=True)
+b.metric("Wind", "4 mph", "2 mph", border=True)
 
-Two (or more) newline characters in a row will result in a hard return.
-'''
-st.markdown(multi)
-import streamlit as st
-
-st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
-st.subheader("This is a subheader with a divider", divider="gray")
-st.subheader("These subheaders have rotating dividers", divider=True)
-st.subheader("One", divider=True)
-st.subheader("Two", divider=True)
-st.subheader("Three", divider=True)
-st.subheader("Four", divider=True)
+c.metric("Humidity", "77%", "5%", border=True)
+d.metric("Pressure", "30.34 inHg", "-2 inHg", border=True)
